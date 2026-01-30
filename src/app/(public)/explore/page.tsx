e1e80fd615d21cd49403b4fac9dc267f95b8194c
elisha111 <elisey.kovalyov@yandex.ru>
2026-01-30 10:47:34 +0300

    client useSearchParams

--- src/app/(public)/explore/page.tsx
+++ src/app/(public)/explore/page.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from "next";
 import { Explore } from "./Explore";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Explore",
 };
 
 export default function ExplorePage() {
-  return <Explore />;
+  return (
+    <Suspense>
+      <Explore />
+    </Suspense>
+  );
 }
