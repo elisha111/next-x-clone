95d83bf522388076f4c8127e0adee14e44884faf
elisha111 <elisey.kovalyov@yandex.ru>
2026-01-30 13:55:31 +0300

    fetch ISR

--- src/app/(public)/shop/isr/page.tsx
+++ src/app/(public)/shop/isr/page.tsx
@@ -1,3 +1,17 @@
-export default function ShopISRPage() {
-  return <div>ISR</div>;
+import { ProductCard, type IProduct } from "@/components/ProductCard";
+import { API_URL } from "@/constants";
+
+// export const revalidate = 300
+
+export default async function ShopISRPage() {
+  const response = await fetch(API_URL, { next: { revalidate: 300 } });
+  const products = (await response.json()) as IProduct[];
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
