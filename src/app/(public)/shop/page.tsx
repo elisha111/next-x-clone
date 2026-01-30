9fa4dc459de3b650987172c9024609408b61d1b6
elisha111 <elisey.kovalyov@yandex.ru>
2026-01-30 12:38:01 +0300

    fetch SSR

--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,3 +1,17 @@
-export default function ShopPage() {
-  return <div>SSR</div>;
+import { ProductCard, type IProduct } from "@/components/ProductCard";
+import { API_URL } from "@/constants";
+
+// export const dynamic = "force-dynamic"
+
+export default async function ShopPage() {
+  const response = await fetch(API_URL, { cache: "no-store" });
+  const products = (await response.json()) as IProduct[];
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
